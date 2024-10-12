fdf0d06e167330dccade95fa3428e93767520ef3
Remi Van Keisbelck <remi@rvkb.com>
2024-10-12 18:21:39 +0200

    bundle name, circle ci config

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,16 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './src/bundle.ts',
-  // devtool: 'inline-source-map',
   target: ['web', 'es6'],
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'findrjs.bundle.js',
+    path: path.resolve(__dirname, 'bundle'),
+    filename: 'findr.js',
   },
   module: {
     rules: [
       {
         test: /\.ts?$/,
         use: 'ts-loader',
-        // exclude: /node_modules/,
       },
     ],
   },
