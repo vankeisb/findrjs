e18026d0d8df9253d62b4723b77fa5e068c32f9e
Remi Van Keisbelck <remi@rvkb.com>
2024-10-12 17:33:05 +0200

    Logger

---
+++ src/Logger.ts
@@ -0,0 +1,29 @@
+export class Logger {
+  enabled: boolean = false;
+
+  info(message: string): void {
+    if (this.enabled) {
+      console.log(message);
+    }
+  }
+
+  warn(message: string): void {
+    if (this.enabled) {
+      console.log('%c ' + message, 'color: orange;');
+    }
+  }
+
+  success(message: string): void {
+    if (this.enabled) {
+      console.log('%c ' + message, 'color: green;');
+    }
+  }
+
+  error(message: string): void {
+    if (this.enabled) {
+      console.log('%c ' + message, 'color: red;');
+    }
+  }
+}
+
+export const LOG: Logger = new Logger();
